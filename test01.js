301859d6d052dd6e3c1ff579a2f5903b0b1eb910
JN803STUDIO <jn803studio@gmail.com>
2025-08-14 18:13:40 +0900

    2023-08-14-목

---
+++ test01.js
@@ -0,0 +1,7 @@
+            let bunho03 = 1; // 값을 숫자로 할당, 변수의 자료형 - 숫자
+            console.log("bunho03 ===============");
+            console.log(typeof bunho01);
+
+            let bunho04 = "1"; // 값을 문자로 할당, 변수의 자료형 - 문자
+            console.log("bunho04 ===============");
+            console.log(typeof bunho02);
